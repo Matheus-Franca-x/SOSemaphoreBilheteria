6e8c7e95f9aca1ae5e514fef0ea3f8c8a8355128
Matheus <matheussss6613@gmail.com>
2023-03-25 13:21:38 -0300

    Login

--- src/controller/ControllBilheteria.java
+++ src/controller/ControllBilheteria.java
@@ -1,5 +1,53 @@
 package controller;
 
-public class ControllBilheteria {
+import java.util.concurrent.Semaphore;
 
+public class ControllBilheteria extends Thread
+{
+	static int bilhete = 100;
+	int id;
+	Semaphore tempo;
+	
+	public ControllBilheteria(int id, Semaphore tempo) 
+	{
+		this.id = id; 
+		this.tempo = tempo;
+		}
+	
+	public void compraBilhete()
+	{
+		new Thread()
+		{
+			public void run()
+			{
+				int tempo = (int)(Math.random() * 1501 ) + 500;
+				try {
+					sleep(tempo);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (tempo <= 1000)
+				{
+					login();
+					buy();
+				}
+				else
+				{
+					System.out.println("Time Out");
+				}
+			}
+		}.start();
+	}
+	
+	public void buy() 
+	{
+		
+	}
+	
+	public void login()
+	{
+		System.out.println(this.id + " Entrou");
+		
+	}
+	
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,19 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.ControllBilheteria;
+
 public class Main {
 
 	public static void main(String[] args)
 	{
+		Semaphore perm = new Semaphore(1);
 		
+		for(int i = 1; i <= 300; i++)
+		{
+			ControllBilheteria control = new ControllBilheteria(i, perm);
+			control.compraBilhete();
+		}
 	}
 }
